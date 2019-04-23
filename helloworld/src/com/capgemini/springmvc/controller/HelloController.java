b2ac5e33b504dbcfbc474bec3753e7c7d5b68ffd
Manvada <mallesh.manvada@capgemini.com>
2019-04-23 08:39:17 +0530

    spring mvc

---
+++ helloworld/src/com/capgemini/springmvc/controller/HelloController.java
@@ -0,0 +1,16 @@
+
+package com.capgemini.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping("/hello-world")
+public class HelloController {
+
+	@RequestMapping("/")
+	public String display() {
+
+		return "index";
+	}
+}
